5281cbcef4869e54b0482d71c5040503048a004c
l-victor-l <vick.23.lopez@gmail.com>
2026-02-08 03:47:27 +0000

    Changed all main with string of each section on the card. Then fiilled the input with an answer to each string section. Also made slight changes to the ascii art.

--- Main.java
+++ Main.java
@@ -1,37 +1,61 @@
 /**
- * Program description.
- * @author Your Name
+ * This program prints a temporary Employment Authorization Card (EAC)
+ * using ASCII art. It demonstrates basic Java variables and output formatting
+ * by inserting user data into a card template.
+ * @author Victor Lopez
  */
 public class Main
 {
 	public static void main(String[] args)
 	{
+		//CONSTANTS SECTION
+		final String TITLE_USA = "UNITED STATES OF AMERICA";
 		//DECLARATION + INITIALIZATION SECTION
-		
+		String surname;
+	String givenName;
+	String uscisNumber;
+	String category;
+	String cardNumber;
+	String countryOfBirth;
+	String terms;
+	String dateOfBirth;
+	String sex;
+	String validFrom;
+	String cardExpires;
+
 		//INPUT SECTION
-		// N/A
-		
+	surname = "Lopez";
+	givenName = "Victor";
+	uscisNumber = "123-456-789";
+	category = "C08";
+	cardNumber = "A1234567890";
+	countryOfBirth = "Tanzania";
+	terms = "DACA APPROVED";
+	dateOfBirth = "05/16/2005";
+	sex = "M";
+	validFrom = "01/01/2026";
+	cardExpires = "01/01/2028";		
 		//CALCULATION SECTION
 		
 		//OUTPUT SECTION
 		System.out.println("╔══════════════════════════════════════════════════════════════════════");
-		System.out.println("║         UNITED STATES OF AMERICA");
+		System.out.println("║         " + TITLE_USA +"");
 		System.out.println("║                                  EMPLOYMENT AUTHORIZATION CARD");
 		System.out.println("║                       Surname");
-		System.out.println("║                       ███████ ");
+		System.out.println("║                       " + surname + " ");
 		System.out.println("║  .----.    .----.     Given Name");
-		System.out.println("║ (   ^  \\  /  ^   )    ███████ ");
+		System.out.println("║ (   o  \\  /  o   )    " + givenName + " ");
 		System.out.println("║        |  |           USCIS#         Category   Card#");
-		System.out.println("║       _/  \\_          ███-███-███    ███        █████████████");
+		System.out.println("║       _/  \\_          " + uscisNumber +"       " + category + "      " + cardNumber + " ");
 		System.out.println("║      (_    _)         Country of Birth");
-		System.out.println("║   ,    `--`    ,      ███████ ");
-		System.out.println("║   \\'-.______.-'/      Terms and Conditions");
-		System.out.println("║    \\          /       ███████ ");
+		System.out.println("║   ,    `--`    ,      " + countryOfBirth + " ");
+		System.out.println("║   \\'-'______'-'/      Terms and Conditions");
+		System.out.println("║    \\          /       " + terms + " ");
 		System.out.println("║     '.--..--.'        Date of Birth   Sex");
-		System.out.println("║       `\"\"\"\"\"`         ██ ███ ████     █");
-		System.out.println("║                       Valid From:     ██/██/██");
-		System.out.println("║                       Card Expires:   ██/██/██");
-		System.out.println("║ ascii art by: jgs     NOT VALID FOR REENTRY TO U.S.");
+		System.out.println("║       `\"\"\"\"\"`         " + dateOfBirth + "       " + sex + "");
+		System.out.println("║                       Valid From:     " + validFrom + "");
+		System.out.println("║                       Card Expires:   " + cardExpires + "");
+		System.out.println("║ ascii art by: VL     NOT VALID FOR REENTRY TO U.S.");
 		System.out.println("╚══════════════════════════════════════════════════════════════════════");
 		//ascii art credit: http://www.oocities.org/spunk1111/bodypart.htm
 	}
